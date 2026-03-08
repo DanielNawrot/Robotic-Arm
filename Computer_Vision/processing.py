5df76764ec2a7a4cf5b13e1de407e7d096883685
DanielNawrot <168451903+DanielNawrot@users.noreply.github.com>
2026-03-08 13:56:26 -0400

    update

--- Computer_Vision/processing.py
+++ Computer_Vision/processing.py
@@ -23,3 +23,6 @@ class Vector:
         cos_theta = max(-1.0, min(1.0, dot_product / mag_product))  # Clamp for safety
         angle_rad = math.acos(cos_theta)
         return math.degrees(angle_rad)
+
+def vector_substract(v1, v2):
+    return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z)
